122db5d429532723b56f21f81628c3d44e619926
Jason Kraft <developer.kraft@gmail.com>
2020-10-24 13:55:03 -0700

    python Add Two Numbers refactor

--- python/0002_add_two_numbers.py
+++ python/0002_add_two_numbers.py
@@ -1,23 +1,20 @@
 # https://leetcode.com/problems/add-two-numbers/
 # O(max(n, m)) time complexity, where n and m are length of respective linked lists
-# O(1) space complexity
+# O(max(n, m) space complexity
 def add_two_numbers(l1, l2):
-    num1 = num2 = i = j = 0
-    while l1 is not None:
-        num1 += l1.val * 10**i
-        i += 1
-        l1 = l1.next
-    while l2 is not None:
-        num2 += l2.val * 10**j
-        j += 1
-        l2 = l2.next
-    sum = num1 + num2
     dummy = curr = ListNode()
-    while sum > 0:
-        curr.next = ListNode()
+    carry = 0
+    while l1 or l2:
+        x = l1.val if l1 else 0
+        y = l2.val if l2 else 0
+        sum = carry + x + y
+        carry = sum // 10
+        curr.next = ListNode(sum % 10)
         curr = curr.next
-        curr.val = sum % 10
-        sum //= 10
+        l1 = l1.next if l1 else l1
+        l2 = l2.next if l2 else l2
+    if carry > 0:
+        curr.next = ListNode(carry)
     return dummy.next
 
 
